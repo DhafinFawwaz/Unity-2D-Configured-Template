645407f27b5cdf18a85e28af7e821bed47feb1b6
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2022-08-04 20:56:47 +0700

    Remove the default value for path

--- Assets/Code/PresetScripts/Encryption.cs
+++ Assets/Code/PresetScripts/Encryption.cs
@@ -5,7 +5,7 @@ using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 public class Encryption : MonoBehaviour
 {
-    string path = "data/data/";
+    string path;
     const string glyphs= "abcdefghijklmnopqrstuvwxyz0123456789";
 
 
