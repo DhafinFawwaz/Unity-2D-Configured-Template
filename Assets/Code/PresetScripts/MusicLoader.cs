529ffd2a0350852eae220f418270f80c5d896f7b
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2022-12-12 10:33:56 +0700

    Put everything in a coroutine

--- Assets/Code/PresetScripts/MusicLoader.cs
+++ Assets/Code/PresetScripts/MusicLoader.cs
@@ -4,27 +4,20 @@ using UnityEngine;
 public class MusicLoader : MonoBehaviour
 {
     [SerializeField] AudioClip _musicClip;
-    void OnEnable()
-    {
-        if(Singleton.Instance == null)
-        {
-            StartCoroutine(DelayOnEnable());
-            return;
-        }
-
-        if(_musicClip == Singleton.Instance.Audio.GetCurrentMusicClip())
-        {}
-        else if(_musicClip != null)
-            Singleton.Instance.Audio.PlayMusic(_musicClip);
-        else 
-            Singleton.Instance.Audio.StopMusic();
-    }
+    void OnEnable() => StartCoroutine(DelayOnEnable());
 
     IEnumerator DelayOnEnable()
     {
-        yield return new WaitForEndOfFrame();
-        if(_musicClip != null)
+        while(Singleton.Instance == null)
+        {
+            Debug.Log("Singleton isn't initialized yet");
+            yield return null;
+        }
+        if(_musicClip == Singleton.Instance.Audio.GetCurrentMusicClip()) // Same music
+        {}
+        else if(_musicClip != null) // Different music
             Singleton.Instance.Audio.PlayMusic(_musicClip);
-        else Singleton.Instance.Audio.StopMusic();
+        else // No music
+            Singleton.Instance.Audio.StopMusic();
     }
 }
