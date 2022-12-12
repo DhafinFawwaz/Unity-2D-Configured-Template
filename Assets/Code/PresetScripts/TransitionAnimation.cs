615e29cbfd0d2559a7bf18d113c5fc100f21b296
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2022-12-12 10:31:07 +0700

    Music fading in isn't necessary

--- Assets/Code/PresetScripts/TransitionAnimation.cs
+++ Assets/Code/PresetScripts/TransitionAnimation.cs
@@ -31,7 +31,7 @@ public class TransitionAnimation : MonoBehaviour
         float t = 0;
         while(t <= 1)
         {
-            t += Time.unscaledDeltaTime/_outDuration;
+            t += Time.unscaledDeltaTime/_inDuration;
             float newY = Mathf.Lerp(_endYScale, _startYScale, Ease.OutQuart(t));
             _orangeSquareRT.localScale = new Vector3(1, newY, 1);
             yield return null;
