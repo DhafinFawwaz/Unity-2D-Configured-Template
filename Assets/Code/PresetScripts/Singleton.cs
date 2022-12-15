06162d488cb897acf7132d0058ddb8cc2da29e60
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2022-12-15 22:46:49 +0700

    Singleton from editor

--- Assets/Code/PresetScripts/Singleton.cs
+++ Assets/Code/PresetScripts/Singleton.cs
@@ -1,6 +1,10 @@
 using UnityEngine;
 using UnityEngine.Audio;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class Singleton : MonoBehaviour
 {
     public TransitionManager Transition;
@@ -12,14 +16,45 @@ public class Singleton : MonoBehaviour
     
     void Awake()
     {
-        if(Instance == null)
-        {
-            Instance = this;
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
+        if(Instance == null)Instance = this;
+
+        else Destroy(gameObject);
+
+#if UNITY_EDITOR
+        if(Application.isPlaying)
+#endif
+
         DontDestroyOnLoad(gameObject);
     }
+    
+
+#if UNITY_EDITOR
+    // So that access to Singleton exist in edit mode
+    void OnEnable()
+    {
+        if(Application.isPlaying)return;
+
+        if(Instance == null)Instance = this;
+        // else DestroyImmediate(gameObject);
+    }
+    public static void LoadSingleton()
+    {
+        if(Singleton.Instance == null)
+        {
+            GameObject singleton = Resources.Load("SINGLETON") as GameObject;
+            if(singleton == null)
+            {
+                Debug.Log("SINGLETON prefab not found in .../Resources/SINGLETON. Please don't remove or move this to other folder.", singleton);
+                return;
+            }
+
+            PrefabUtility.InstantiatePrefab(singleton);
+            if(Singleton.Instance == null)
+            {
+                Debug.Log("Something went wrong with ", Singleton.Instance);
+                return;
+            }
+        }
+    }
+#endif
 }
